fa368f2740cb9502a2acd681ef37c3d5573c27cc
richlloydmiles <richlloydmiles@gmail.com>
2017-03-22 21:36:51 +0200

    initial test

---
+++ README.md
@@ -0,0 +1,7 @@
+## Initial phpUnit test
+
+`phpunit --bootstrap src/User.php tests/UserTest`
+
+create localhost with -
+
+`php -S localhost:8888`
---
+++ tests/UserTest.php
@@ -0,0 +1,20 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @covers Email
+ */
+final class EmailTest extends TestCase
+{
+    // test the talk method
+  public function testTalk() {
+        // make an instance of the user
+    $user = new User();
+
+        // use assertEquals to ensure the greeting is what you
+    $expected = "Hello world!";
+    $actual = $user->talk();
+    $this->assertEquals($expected, $actual);
+  }
+}
